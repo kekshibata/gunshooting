3471397d0d67d31fb6a634525679e376f3560f59
Koki Shibata <>
2021-05-20 14:48:10 +0900

    ゲームヘッダーリンク化

--- src/components/game-header.jsx
+++ src/components/game-header.jsx
@@ -14,11 +14,15 @@ const GameHeader = ({
 }) => (
   <header>
     <div className={titleHeader}>
-      <ImgixGatsbyImage src={iconSource} layout="constrained" width={35} height={35} className="block rounded-md shadow-sm z-10" />
-      <div className="ml-2 font-bold leading-5 overflow-visible">
-        {gameName}
-        攻略wiki
-      </div>
+      <Link to={`/${slug}`}>
+        <ImgixGatsbyImage src={iconSource} layout="constrained" width={35} height={35} className="block rounded-md shadow-sm z-10" />
+      </Link>
+      <Link to={`/${slug}`}>
+        <div className="ml-2 font-bold leading-5 overflow-visible">
+          {gameName}
+          攻略wiki
+        </div>
+      </Link>
     </div>
     <ImgixGatsbyImage src={headerSource} layout="constrained" height={90} aspectRatio={16 / 5} className="w-full block align-middle" />
     <ul className={navMenu}>
