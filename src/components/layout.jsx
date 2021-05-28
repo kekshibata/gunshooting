2c27f4ef98d714e10ada5d23bb6d9edf1ace1e6b
Koki Shibata <>
2021-05-28 10:54:31 +0900

    page transiton実装

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import Header from './header';
 import Footer from './footer';
-
-import {
-  background,
-} from './layout.module.css';
 
 const Layout = (props) => (
   <>
     <div className="max-w-lg mx-auto bg-white">
       <Header />
-      <main>{props.children}</main>
+      <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{
+          type: 'spring',
+          duration: 0.5,
+        }}
+      >
+        {props.children}
+      </motion.main>
       <Footer />
     </div>
     {/* <div className={background} /> */}
